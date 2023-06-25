88a2fa715e36bb4085361d0cfcdcce4d9ceca1a9
P-axel <annonier.pierreaxel@gmail.com>
2023-06-25 18:29:14 +0200

    ready api

--- papi/src/Entity/Customer.php
+++ papi/src/Entity/Customer.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\CustomerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CustomerRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ * @ApiResource
  */
 class Customer
 {
