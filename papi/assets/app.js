6f0e83bf7cd72f8ffd4a2d51c3de536b86b050e3
P-axel <annonier.pierreaxel@gmail.com>
2023-06-25 21:28:47 +0200

    javascript ok

---
+++ papi/assets/app.js
@@ -0,0 +1,16 @@
+
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+console.log("je t'aime");
+
+
+
+
+export default function(app) {
+    return `Yo yo ${app} - welcome to Encore!`;
+};
